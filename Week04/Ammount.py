bc73df523e4d6389c76dd6788a67fd2d921e9e3e
Sorrou <33961291+Sorrou@users.noreply.github.com>
2019-02-19 10:45:03 +0300

    Create Ammount.py

---
+++ Week04/Ammount.py
@@ -0,0 +1,32 @@
+class Value:
+
+    def __init__(self):
+        self.value = None
+    
+    @staticmethod
+    def _prepare_value(value, commission):
+        return value - (value * commission)
+
+    def __get__(self, obj, obj_type):
+        return self.value
+    
+    def __set__(self, obj, value):
+        self.value = self._prepare_value(value, obj.commission)
+
+class Account:
+    amount = Value()
+
+    def __init__(self, commission):
+        self.commission = commission
+
+'''
+class Value:
+    def __init__(self):
+        self.amount = 0
+
+    def __get__(self, obj, obj_type):
+        return self.amount
+
+    def __set__(self, obj, value):
+        self.amount = value - value * obj.commission
+'''        
